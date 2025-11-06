fc19a47789ebb3fa05afff821b4cf63d44b1f463
Elisa Rocha Eleotério <ere24@atlas>
2025-11-06 15:23:40 -0300

    [add] achar k_vizinhos

--- p4/kdtree.c
+++ p4/kdtree.c
@@ -2,18 +2,19 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <math.h>
 
 #include "kdtree.h"
 
 // struct nodo {
-//     int64_t *vetchave;
+    //     int64_t *vetchave;
 //     int64_t classe;
 //     struct nodo *fe;
 //     struct nodo *fd;
 //     struct nodo *pai;
 // };
 
-
+// FUNÇÕES EXTRAS
 struct nodo *criar_raiz() {
     struct nodo *raiz = malloc(sizeof(struct nodo));
     if (!raiz) {
@@ -25,45 +26,6 @@ struct nodo *criar_raiz() {
     raiz->vetchave = NULL;
 
     return raiz;
-}
-
-
-// árvore não precisa ser balanceada
-void criar_kdtree(struct nodo **raiz, uint64_t n, uint64_t k) {
-    if (!raiz) {
-        matarProgramaPonteiroNulo();
-    }
-    
-    //Ler todos os nodos
-    for (size_t i = 0; i < n; i++) {
-        int64_t *vetchave = malloc(k * sizeof(int64_t));
-        if (!vetchave) {
-            matarProgramaFaltaMemoria();
-        }
-        // Ler todos os pontos do nodo
-        for (size_t j = 0; j < k; i++) {
-            scanf("%ld", vetchave[j]);
-        }
-        // Ler classe
-        int64_t classe;
-        scanf("%ld", &classe);
-        
-        // Inserir o vetor de pontos na árvore
-        inserir(raiz, vetchave, classe, k);        
-    }
-}    
-struct nodo *buscar_kdtree(struct nodo *raiz, int64_t *vetchave);
-int64_t *encontrar_z_vizinhos(struct nodo *raiz, int64_t *vetchave);
-
-// FUNÇÕES DE ERRO
-void matarProgramaFaltaMemoria() {
-    fprintf(stderr, "Erro por falta de memória\n");
-    exit(1);
-}
-
-void matarProgramaPonteiroNulo() {
-    fprintf(stderr, "Erro por ponteiro nulo\n");
-    exit(1);
 }
 
 struct nodo *inserir(struct nodo **raiz, int64_t *vetchave, int64_t classe, uint64_t dimesoes) {
@@ -78,7 +40,7 @@ struct nodo *inserir(struct nodo **raiz, int64_t *vetchave, int64_t classe, uint
     novo->vetchave = vetchave;
     novo->fe = NULL;
     novo->fd = NULL;
-
+    
     struct nodo *atual = raiz;
     struct nodo *pai = NULL;
 
@@ -103,4 +65,98 @@ struct nodo *inserir(struct nodo **raiz, int64_t *vetchave, int64_t classe, uint
         }
     }
     return novo;
+}
+
+
+// FUNÇÕES OBRIGATÓRIAS
+// árvore não precisa ser balanceada
+void criar_kdtree(struct nodo **raiz, uint64_t n, uint64_t k) {
+    if (!raiz) {
+        matarProgramaPonteiroNulo();
+    }
+    
+    //Ler todos os nodos
+    for (size_t i = 0; i < n; i++) {
+        int64_t *vetchave = malloc(k * sizeof(int64_t));
+        if (!vetchave) {
+            matarProgramaFaltaMemoria();
+        }
+        // Ler todos os pontos do nodo
+        for (size_t j = 0; j < k; i++) {
+            scanf("%ld", vetchave[j]);
+        }
+        // Ler classe
+        int64_t classe;
+        scanf("%ld", &classe);
+        
+        // Inserir o vetor de pontos na árvore
+        inserir(raiz, vetchave, classe, k);        
+    }
+}    
+
+
+struct nodo *buscar_kdtree(struct nodo *raiz, int64_t *vetchave, uint64_t coord, uint64_t dimensoes) {
+    if (!vetchave) {
+        matarProgramaPonteiroNulo();
+    }
+    
+    if (!raiz || raiz->vetchave == NULL) {
+        return raiz;
+    }
+    
+    if (vetchave[coord] < raiz->vetchave[coord]) {
+        return buscar(raiz->fe, vetchave, (coord + 1) % dimensoes, dimensoes);
+    }
+    return buscar(raiz->fd, vetchave, (coord + 1) % dimensoes, dimensoes);
+}
+
+int64_t *encontrar_z_vizinhos(struct nodo **raiz, uint64_t coord, uint64_t dimensoes, int64_t *vetchave) {
+    if (!raiz) {
+        matarProgramaPonteiroNulo();
+    }
+    
+    if (*raiz->fd == NULL && *raiz->fe == NULL) {
+        int64_t distancia = sqrt(pow())
+    }
+    
+
+
+
+
+    função vizinhoMaisProx(r, coord, k, ponto, melhor)
+        
+    
+    se r é folha:
+            dist = ‖r - ponto‖2
+            se dist < melhor.dist:
+                retorne (r, dist)
+            senão:
+                retorne melhor
+        se ponto[coord] < r.vetchave[coord]
+            prim = r.fe
+            sec = r.fd
+        senão
+            prim = r.fd
+            sec = r.fe
+        melhor = vizinhoMaisProx(prim,(coord+1) mod k, k, ponto, melhor)
+        se ‖r - ponto‖2 < melhor.dist
+            melhor = (r, dist)
+        se abs(r.vetchave[coord] - ponto[coord]) < melhor.dist:
+            novo = vizinhoMaisProx(sec,(coord+1) mod k, k, ponto, melhor)
+            se novo.dist < melhor.dist:
+                retorne novo
+        retorne melhor
+}
+
+
+
+// FUNÇÕES DE ERRO
+void matarProgramaFaltaMemoria() {
+    fprintf(stderr, "Erro por falta de memória\n");
+    exit(1);
+}
+    
+void matarProgramaPonteiroNulo() {
+    fprintf(stderr, "Erro por ponteiro nulo\n");
+    exit(1);
 }
